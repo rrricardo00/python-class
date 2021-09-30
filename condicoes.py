9cb2841105c6f01042d538936adfa32b65cdc6d4
rrricardo00 <rrricardo00@yahoo.com.br>
2021-09-30 01:21:41 -0300

    v1.0

---
+++ condicoes.py
@@ -0,0 +1,8 @@
+tempo = int(input("Quantos anos tem seu carro? \n"))
+
+# if tempo <= 3:
+#     print("Carro novo")
+# else:
+#     print("Carro com mais de 3 anos")
+
+print('carro novo' if tempo <= 3 else 'carro velho')

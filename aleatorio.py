06bd8a361f75b26d0be23bfb4f5648574ff4bf7b
Ricardo Rosa <rrricardo00@yahoo.com.br>
2021-10-07 10:03:28 -0300

    py

---
+++ aleatorio.py
@@ -0,0 +1,7 @@
+from random import randint
+
+computador = randint(0, 5)
+print("Pensei no numero {}" .format(computador))
+print("Vou pensar em um número entre 0 e 5\n")
+jogador = int(input("Em que número eu pensei? "))
+print(jogador)
